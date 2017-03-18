191507db1e0afc4e4a9a1a332e8dd7dbf69b9a62
Daniel Grams <grams.daniel@gmail.com>
2017-03-18 20:55:34 +0100

    LocationManager für das Einlesen von Koordinaten hinzugefügt

--- app/src/main/java/parkplatz/goslar/de/parkplatzgoslar/MainActivity.java
+++ app/src/main/java/parkplatz/goslar/de/parkplatzgoslar/MainActivity.java
@@ -1,13 +1,114 @@
 package parkplatz.goslar.de.parkplatzgoslar;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    private static final String[] LOCATION_PERMS={
+            Manifest.permission.ACCESS_FINE_LOCATION
+    };
+
+    private static final int INITIAL_REQUEST=1337;
+    private static final int LOCATION_REQUEST=INITIAL_REQUEST+1;
+
+    private LocationManager locationManager;
+
+    private TextView txtLatitude;
+    private TextView txtLongitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtLatitude = (TextView)findViewById(R.id.txtLat);
+        txtLongitude = (TextView)findViewById(R.id.txtLon);
+
+        if (!canAccessLocation()) {
+            ActivityCompat.requestPermissions(this, LOCATION_PERMS, INITIAL_REQUEST);
+        }
+        else {
+            startTracking();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+
+        switch(requestCode) {
+
+            case LOCATION_REQUEST:
+                if (canAccessLocation()) {
+                    startTracking();
+                }
+                else {
+
+                }
+                break;
+        }
+    }
+
+    private boolean canAccessLocation() {
+        return(hasPermission(Manifest.permission.ACCESS_FINE_LOCATION));
+    }
+
+    private boolean hasPermission(String perm) {
+        return(PackageManager.PERMISSION_GRANTED== ContextCompat.checkSelfPermission(getBaseContext(), perm));
+    }
+
+    private void startTracking()
+    {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        try
+        {
+            locationManager.requestLocationUpdates( LocationManager.GPS_PROVIDER,
+                    2000,
+                    10, this);
+        }
+        catch (SecurityException exception)
+        {
+            Toast.makeText(getBaseContext(), exception.getLocalizedMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        String msg = "New Latitude: " + location.getLatitude()
+                + "New Longitude: " + location.getLongitude();
+
+        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        Toast.makeText(getBaseContext(), "Gps is turned on!! ",
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+        startActivity(intent);
+        Toast.makeText(getBaseContext(), "Gps is turned off!! ",
+                Toast.LENGTH_SHORT).show();
     }
 }
